4f039cb544c4f416788d87c19f4f4a0dde10ccfa
Garbanzos-con-chorizo <garbanchin@gmail.com>
2025-07-03 12:57:55 +0200

    Merge main with security enhancements

--- jarvis/__init__.py
+++ jarvis/__init__.py
@@ -3,5 +3,6 @@
 from jarvis_core import JarvisCore, ChatGPTModule
 from .gui import JarvisGUI
 from .lab import LabModule
+from .data import DataManager
 
-__all__ = ["JarvisCore", "JarvisGUI", "LabModule", "ChatGPTModule"]
+__all__ = ["JarvisCore", "JarvisGUI", "LabModule", "ChatGPTModule", "DataManager"]
